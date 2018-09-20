1b0f00f29a9b84a99808d3fb3eff72fc79a5141e
Michael Scarpace <mscarpace@deloitte.com>
2018-09-20 10:31:01 -0400

    first commit

---
+++ src/mouse/Mouse.java
@@ -0,0 +1,18 @@
+package mouse;
+
+import java.awt.*;
+import java.util.*;
+public class Mouse{
+
+    public static void main(String[] args) throws Exception{
+        Robot hal = new Robot();
+        Random random = new Random();
+        while(true){
+            hal.delay(1000 * 60);
+            int x = random.nextInt() % 640;
+            int y = random.nextInt() % 480;
+            hal.mouseMove(x,y);
+            System.out.println("Mouse moved to " + x + " " + y);
+        }
+    }
+} 
